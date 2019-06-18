61bc696959aa2257b0a2a99839c9968389356b28
Black <psgrepjava@gmail.com>
2019-06-18 10:40:11 +1000

    Removed redundant script

--- bin/cli.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CLI_ASSEMBLY="${BASH_SOURCE%/*/*}/grapheus-distributive/grapheus-dist-cli/target/grapheus-cli"
-
-: ${RXSERVER_PORT:=8081}
-JAVA_OPTS="-Dgrapheus.server.baseURL=http://127.0.0.1:${RXSERVER_PORT}/grapheus" \
-    ${CLI_ASSEMBLY}/grapheus-cli.sh "$@"
