da3d7a2049339e696fd76fb8ffb224814bdc246a
Black <psgrepjava@gmail.com>
2019-06-25 21:35:59 +1000

    script with 'release' spell

---
+++ bin/release.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+##################################### Globals ###############################################
+set -u
+
+SCRIPTS_ROOT="${BASH_SOURCE%/*}"
+
+. ${SCRIPTS_ROOT}/include/log.sh build.log
+. ${SCRIPTS_ROOT}/include/version-utils.sh
+
+CMD="${1-}"
+
+TARGET_FOLDER="$(pwd)/target"
+RELEASE_CHECKOUT_FOLDER="${TARGET_FOLDER}/release-checkout"
+
+##################################### Functions ###############################################
+
+# Checks out latest release tag into separate folder
+get_release() {
+    mkdir -p "${TARGET_FOLDER}"
+    rm -rf "${RELEASE_CHECKOUT_FOLDER}"
+    
+    git clone . "${RELEASE_CHECKOUT_FOLDER}"
+
+    (
+        cd "${RELEASE_CHECKOUT_FOLDER}" || die "Cannot navigate to folder ${RELEASE_CHECKOUT_FOLDER}"
+        git checkout "$(latest_tag)"
+    )
+    
+    echo "Release is checked out in '${RELEASE_CHECKOUT_FOLDER}'"
+}
+
+# Performs release:prepare
+perform_prepare() {
+    local last_release_version=$(release_version)
+    read -p "New version (last released was ${last_release_version}): " new_version
+    echo "Preparing new release v ${new_version}"
+
+    mvn clean release:prepare -Darguments=-DskipTests -DreleaseVersion="${new_version}"
+    
+    get_release
+}
+
+case "${CMD}" in
+    get) get_release ;;
+    perform) perform_prepare ;;
+    *)
+        echo "Usage:"
+        echo "  $0 {get|perform}"
+esac
