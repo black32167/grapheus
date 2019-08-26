1632fad66c985098ed764d6a9dbf3ade502acb4b
Black <psgrepjava@gmail.com>
2019-08-26 22:39:14 +1000

    Shared Http codes

---
+++ grapheus-common/src/main/java/org/grapheus/common/HttpCodes.java
@@ -0,0 +1,10 @@
+package org.grapheus.common;
+
+public final class HttpCodes {
+    public final static int OK = 200;
+    public static final int NO_CONTENT = 204;
+
+    public final static int NOT_FOUND = 404;
+
+    private HttpCodes() {}
+}
