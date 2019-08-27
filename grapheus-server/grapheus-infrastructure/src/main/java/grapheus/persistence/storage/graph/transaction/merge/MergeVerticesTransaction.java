9510450f979f33e6541fa8334a0ed181c40dd30c
Black <psgrepjava@gmail.com>
2019-08-27 19:21:58 +1000

    Using structured response for vertices merging operation

--- grapheus-server/grapheus-infrastructure/src/main/java/grapheus/persistence/storage/graph/transaction/merge/MergeVerticesTransaction.java
+++ grapheus-server/grapheus-infrastructure/src/main/java/grapheus/persistence/storage/graph/transaction/merge/MergeVerticesTransaction.java
@@ -3,18 +3,13 @@
  */
 package grapheus.persistence.storage.graph.transaction.merge;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-
 import grapheus.persistence.storage.graph.transaction.FoxxEndpointNames;
 import grapheus.persistence.storage.graph.transaction.FoxxSupport;
 import org.springframework.stereotype.Service;
 
-import com.arangodb.model.TransactionOptions;
-
-import grapheus.persistence.storage.graph.GraphNameUtils;
-import grapheus.persistence.storage.graph.transaction.ServerSideTransaction;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author black
@@ -22,12 +17,16 @@ import grapheus.persistence.storage.graph.transaction.ServerSideTransaction;
  */
 @Service
 public class MergeVerticesTransaction extends FoxxSupport {
+    public static class MergeResult {
+        String newVertexKey;
+    }
     public String merge(String grapheusUserKey, String graphId, String newVertexName, Collection<String> verticesIds) {
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("graphId", graphId);
         parameters.put("newVertexName", newVertexName);
         parameters.put("verticesIds", String.join(",", verticesIds));
-        
-        return invokeFoxx(FoxxEndpointNames.MERGE_VERTICES, parameters, String.class);
+
+        MergeResult mergeResult = invokeFoxx(FoxxEndpointNames.MERGE_VERTICES, parameters, MergeResult.class);
+        return mergeResult.newVertexKey;
     }
 }
